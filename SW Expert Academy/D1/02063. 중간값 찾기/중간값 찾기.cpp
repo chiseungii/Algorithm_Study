0ef0919c9b1b7df7e183ed113a66ceb5bbe370ff
tube <43616130+chiseungii@users.noreply.github.com>
2024-03-29 20:33:44 +0900

    Create 중간값 찾기.cpp

---
+++ SW Expert Academy/D1/02063. 중간값 찾기/중간값 찾기.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(){
+	int n; cin>>n;
+	vector<int> nums(n);
+	for(int i=0; i<n; cin>>nums[i++]);
+	
+	sort(nums.begin(), nums.end());
+	cout<<nums[n/2];
+}

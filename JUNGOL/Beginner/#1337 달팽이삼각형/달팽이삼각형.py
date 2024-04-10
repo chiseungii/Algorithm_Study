39146ba04586e8b7e5fe60d04034e7d04eb516aa
tube <43616130+chiseungii@users.noreply.github.com>
2024-04-10 17:09:47 +0900

    Create 달팽이삼각형.py

---
+++ JUNGOL/Beginner/#1337 달팽이삼각형/달팽이삼각형.py
@@ -0,0 +1,32 @@
+def main():
+    n = int(input())
+    snail = [[0 for i in range(n)] for i in range(n)]
+
+    cnt,dir = n,0
+    x,y,num = -1,-1,0
+    while cnt > 0:
+        if dir == 0:
+            for i in range(cnt):
+                x,y = x+1,y+1
+                snail[x][y] = num
+                num = (num+1)%10
+        elif dir == 1:
+            for i in range(cnt):
+                y -= 1
+                snail[x][y] = num
+                num = (num+1)%10
+        else:
+            for i in range(cnt):
+                x -= 1
+                snail[x][y] = num
+                num = (num+1)%10
+
+        dir = (dir+1)%3
+        cnt -= 1
+
+    for i in range(n):
+        for j in range(i+1):
+            print(snail[i][j], end=' ')
+        print()
+
+main()

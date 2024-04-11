25974d54b5d387f34712c33cdea853aca355af66
tube <43616130+chiseungii@users.noreply.github.com>
2024-04-11 23:51:27 +0900

    Create 그릇.py

---
+++ JUNGOL/Beginner/#2604 그릇/그릇.py
@@ -0,0 +1,12 @@
+def main():
+    s = input()
+
+    answer = 0
+    for i in range(len(s)):
+        if i == 0: answer += 10
+        elif s[i] == s[i-1]: answer += 5
+        else: answer += 10
+
+    print(answer)
+
+main()

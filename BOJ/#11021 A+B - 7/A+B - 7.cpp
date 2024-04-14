be92ba4e297c7e66ae1f9ed2eee5463d01751af3
tube <43616130+chiseungii@users.noreply.github.com>
2024-04-14 19:11:34 +0900

    Create A+B - 7.cpp

---
+++ BOJ/#11021 A+B - 7/A+B - 7.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T; cin>>T;
+    for(int tc=0; tc<T; tc++) {
+        int A,B; cin>>A>>B;
+        printf("Case #%d: %d\n", tc+1, A+B);
+    }
+}

56fe6fb95426492c5133e84d5ceb9df480b74c6d
tube <43616130+chiseungii@users.noreply.github.com>
2024-04-17 22:51:07 +0900

    Create 피보나치 수.cpp

---
+++ BOJ/#2747 피보나치 수/피보나치 수.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n; cin>>n;
+
+    if(n == 0) cout<<0;
+    else if(n == 1) cout<<1;
+    else {
+        int a=0, b=1, c=1;
+        for(int i=3; i<=n; i++) {
+            a = b;
+            b = c;
+            c = a+b;
+        }
+
+        cout<<c;
+    }
+}
